cc211d7ca79d75479acf37489913338e9a2f097f
Christian Zube <christian.f.g.zube@gmail.com>
2023-02-07 21:04:14 +0100

    worked on new offer

--- src/components/Offers/New/styles.js
+++ src/components/Offers/New/styles.js
@@ -1,3 +1,24 @@
 import styled from "styled-components";
 
-export const ContainernewOffer = styled.div``
+export const ContainernewOffer = styled.div`
+.fontSize{
+    font-size: ${(props) => props.theme.size.xxxs};
+    @media (orientation: landscape){
+        font-size: ${(props) => props.theme.size.xxxxs};
+    }
+    @media (orientation: portrait) and (min-width: 768px){
+        font-size: ${(props) => props.theme.size.s};
+    }
+    @media (orientation: landscape) and (min-height: 768px){
+        font-size: ${(props) => props.theme.size.s};
+    }
+    @media (orientation: portrait) and (min-width: 1024px){
+        font-size: ${(props) => props.theme.size.l};
+    }
+    @media (orientation: landscape) and (min-height: 1024px){
+        font-size: ${(props) => props.theme.size.l};
+    }
+}
+.offerContentSection{
+    margin-top: ${(props) => props.theme.size.xxxxxxl};
+}`
