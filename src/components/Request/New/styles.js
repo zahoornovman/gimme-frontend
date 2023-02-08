13e0ab44265d9b5385f1de9d25c6033570aaf78b
Christian Zube <christian.f.g.zube@gmail.com>
2023-02-08 11:51:41 +0100

    new offer and new request completed except of file upload

--- src/components/Request/New/styles.js
+++ src/components/Request/New/styles.js
@@ -1,3 +1,33 @@
 import styled from "styled-components";
 
-export const ContainerNewRequest = styled.div``
+export const ContainerNewRequest = styled.div`
+.fontSize{
+    font-size: ${(props) => props.theme.size.xxxs};
+    @media (orientation: landscape){
+        font-size: ${(props) => props.theme.size.xxxxs};
+    }
+    @media (orientation: portrait) and (min-width: 768px){
+        font-size: ${(props) => props.theme.size.s};
+    }
+    @media (orientation: landscape) and (min-height: 768px){
+        font-size: ${(props) => props.theme.size.s};
+    }
+    @media (orientation: portrait) and (min-width: 1024px){
+        font-size: ${(props) => props.theme.size.l};
+    }
+    @media (orientation: landscape) and (min-height: 1024px){
+        font-size: ${(props) => props.theme.size.l};
+    }
+}
+.accessDenied{
+    margin-top: ${(props) => props.theme.size.xxxxxxxl};
+}
+.created{
+    margin-top: ${(props) => props.theme.size.xxxxxxxl};
+}
+.error{
+    margin-top: ${(props) => props.theme.size.xxxxxxxl};
+}
+.requestContentSection{
+    margin-top: ${(props) => props.theme.size.xxxxxxl};
+}`
