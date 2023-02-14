65ad0ad2a269500300b4ba1b031a5178d1bedf80
Zahoor Novman <zahoor.novman@gmail.com>
2023-02-14 14:14:33 +0100

    multiple menus added for requests and offers

--- src/elements/Menu2/index.js
+++ src/elements/Menu2/index.js
@@ -3,6 +3,8 @@ import { useNavigate, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { ReactComponent as CogIcon } from "../../images/cog.svg";
 import { ReactComponent as ChevronIcon } from "../../images/chevron.svg";
+import { CSSTransition } from "react-transition-group";
+import { useState } from "react";
 
 function Menu2(props) {
   const status = useSelector((state) => state.user.loading);
@@ -18,9 +20,14 @@ function Menu2(props) {
 export default Menu2;
 
 function DropdownMenu() {
+  const [activeMenu, setActiveMenu] = useState("main"); // offers, requests
   function DropdownItem(props) {
     return (
-      <a href="#" className="menu-item">
+      <a
+        href="#"
+        className="menu-item"
+        onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}
+      >
         <span className="icon-button">{props.leftIcon}</span>
         {props.children}
         <span className="icon-right">{props.rightIcon}</span>
@@ -30,11 +37,50 @@ function DropdownMenu() {
 
   return (
     <div className="dropdown">
-      <DropdownItem leftIcon={<CogIcon />} rightIcon={<ChevronIcon />}>
-        My Profile
-      </DropdownItem>
-      <DropdownItem rightIcon={<ChevronIcon />}> All Offers</DropdownItem>
-      <DropdownItem rightIcon={<ChevronIcon />}>All Requests</DropdownItem>
+      <CSSTransition
+        in={activeMenu === "main"}
+        unmountOnExit
+        timeout={250}
+        classNames="menu-primary"
+      >
+        <div className="menu">
+          <DropdownItem rightIcon={<ChevronIcon />}>My Profile</DropdownItem>
+          <DropdownItem rightIcon={<ChevronIcon />} goToMenu="offers">
+            Offers
+          </DropdownItem>
+          <DropdownItem rightIcon={<ChevronIcon />} goToMenu="requests">
+            Requests
+          </DropdownItem>
+        </div>
+      </CSSTransition>
+
+      <CSSTransition
+        in={activeMenu === "offers"}
+        unmountOnExit
+        timeout={250}
+        classNames="menu-secondary"
+      >
+        <div className="menu">
+          <DropdownItem goToMenu="main"> Go Back</DropdownItem>
+          <DropdownItem> All Offers</DropdownItem>
+          <DropdownItem> My Offers</DropdownItem>
+          <DropdownItem>Create New Offer</DropdownItem>
+        </div>
+      </CSSTransition>
+
+      <CSSTransition
+        in={activeMenu === "requests"}
+        unmountOnExit
+        timeout={250}
+        classNames="menu-secondary"
+      >
+        <div className="menu">
+          <DropdownItem goToMenu="main"> Go Back</DropdownItem>
+          <DropdownItem> All Requests</DropdownItem>
+          <DropdownItem> My Requests</DropdownItem>
+          <DropdownItem>Create New Request</DropdownItem>
+        </div>
+      </CSSTransition>
     </div>
   );
 }
