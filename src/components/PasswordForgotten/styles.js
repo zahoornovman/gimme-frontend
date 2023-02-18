097eb5e654058214664dd858590c0acdc6fad438
Aleksandar <aceristovski@gmail.com>
2023-02-18 14:10:31 +0100

    more css changes

--- src/components/PasswordForgotten/styles.js
+++ src/components/PasswordForgotten/styles.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 export const ContainerPasswordForgotten = styled.div`
 height: 100vh;
-width: 100vw;
 display: flex;
 align-items: center;
 justify-content: center;
--- src/components/Request/New/styles.js
+++ src/components/Request/New/styles.js
@@ -90,6 +90,22 @@ align-items: center;
 
 }
 
+.createdButton {
+  display: flex;
+}
+
+.createdButtons button {
+  font-size: 15px;
+  height: 40px;
+  width: 140px;
+  border-style: none;
+  text-align: center;
+  color: ${(props) => props.theme.colors.color5};
+  border-radius: 10px;
+  margin-left: 20px;
+  margin-top: 30px;
+}
+
 
 .buttonStyle{
     font-size: 15px;
